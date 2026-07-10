6998413c272d85350bd9ad902527bc7b9ed49a69
HunterTigerX <HunterTigerX@gmail.com>
2026-07-11 00:12:32 +0300

    feat: update search results 11.07.2026  0:12:31

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import songsData from './data.js';
+import searchResults from './searchResponce.js';
 import { type PodcastData } from './types.js';
 
 const grid = document.getElementById('podcastGrid');
@@ -25,7 +26,6 @@ let timerId: number | undefined;
 // }
 
 // const podcastApiSearch = new App();
-
 
 function fetchPodcasts(query: string | void): void {
   console.log(query);
@@ -60,19 +60,79 @@ if (searchBoxDom) {
   });
 }
 
-function renderPodcasts(data: PodcastData) {
-  if (grid && counter) {
+function renderPodcasts() {
+  if (grid && counter && searchBoxDom) {
     grid.innerHTML = '';
+    const searchValue = (searchBoxDom as HTMLInputElement).value;
+    console.log(searchValue);
 
+    if (searchValue) {
+      renderSearchPage();
+    } else {
+      renderMainPage();
+    }
+  } else {
+    console.error('Grid or Counter is missing on the page');
+  }
+}
+
+function renderSearchPage() {
+  if (grid && counter) {
+    console.log('v1');
+    // We have text in the search bar
     // We check if we have any cards in the data.ts
-    if (!data || !data.podcasts) {
+    if (!searchResults || !searchResults.results) {
       grid.innerHTML = '<p>Podcasts are missing</p>';
       counter.textContent = '0 podcasts';
       return;
     }
-
     // We create cards and fill them with data
-    data.podcasts.forEach((item, index) => {
+    searchResults.results.forEach((item, index) => {
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      const cover = document.createElement('div');
+      cover.className = 'card__cover';
+
+      if (item.image) {
+        cover.style.backgroundImage = `url('${item.image}')`;
+      } else {
+        console.log(`Image ling is missing for the image ${index}`);
+        cover.style.backgroundImage = 'none';
+      }
+
+      const title = document.createElement('div');
+      title.className = 'card__title';
+      title.textContent = item.title_original || '';
+
+      const author = document.createElement('div');
+      author.className = 'card__author';
+      author.textContent = item.podcast.publisher_original || '';
+
+      card.appendChild(cover);
+      card.appendChild(title);
+      card.appendChild(author);
+      grid.appendChild(card);
+    });
+
+    // Podcast counter
+    const count = searchResults.results.length;
+    const word = count % 10 === 1 && count % 100 !== 11 ? 'Podcast' : 'Podcasts';
+    counter.textContent = `${count} ${word}`;
+  }
+}
+
+function renderMainPage() {
+  if (grid && counter) {
+    // Search bar is empty
+    // We check if we have any cards in the data.ts
+    if (!songsData || !songsData.podcasts) {
+      grid.innerHTML = '<p>Podcasts are missing</p>';
+      counter.textContent = '0 podcasts';
+      return;
+    }
+    // We create cards and fill them with data
+    songsData.podcasts.forEach((item, index) => {
       const card = document.createElement('div');
       card.className = 'card';
 
@@ -100,13 +160,10 @@ function renderPodcasts(data: PodcastData) {
       grid.appendChild(card);
     });
 
-    // Обновляем счётчик
-    const count = data.podcasts.length;
+    // Podcast counter
+    const count = songsData.podcasts.length;
     const word = count % 10 === 1 && count % 100 !== 11 ? 'Podcast' : 'Podcasts';
     counter.textContent = `${count} ${word}`;
-  } else {
-    console.error('Grid or Counter is missing on the page');
   }
 }
-
-renderPodcasts(songsData);
+renderPodcasts();
--- src/types.ts
+++ src/types.ts
@@ -65,3 +65,48 @@ export interface LookingFor {
   sponsors: boolean;
   cross_promotion: boolean;
 }
+
+export interface SearchResponse {
+    took: number;
+    count: number;
+    total: number;
+    results: Episode[];
+    next_offset: number;
+}
+
+
+interface Episode {
+    id: string;
+    rss: string;
+    link: string;
+    audio: string;
+    image: string;
+    podcast: searchPodcast;
+    itunes_id: number;
+    thumbnail: string;
+    pub_date_ms: number;
+    guid_from_rss: string;
+    title_original: string;
+    listennotes_url: string;
+    audio_length_sec: number;
+    explicit_content: boolean;
+    title_highlighted: string;
+    description_original: string;
+    description_highlighted: string;
+    transcripts_highlighted: []; 
+}
+
+
+interface searchPodcast {
+    id: string;
+    image: string;
+    genre_ids: number[];
+    thumbnail: string;
+    listen_score: number;
+    title_original: string;
+    listennotes_url: string;
+    title_highlighted: string;
+    publisher_original: string;
+    publisher_highlighted: string;
+    listen_score_global_rank: string;
+}
